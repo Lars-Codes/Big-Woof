906280525a7c4e19ce04530300c0626395ab9ab3
lars-codes <lara.palombi@gmail.com>
2025-03-28 16:10:29 -0400

    added sqlite db definition

--- server/app.py
+++ server/app.py
@@ -1,9 +1,29 @@
-from flask import Flask 
+from flask import Flask, jsonify, request, Response, url_for
+from models.db import db
+from flask_cors import CORS
+from dotenv import load_dotenv
+import os 
+from flask_migrate import Migrate
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+CORS(app)
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        res = Response()
+        res.headers['X-Content-Type-Options'] = '*'
+        return res
+    
+load_dotenv()
+basedir = os.path.abspath(os.path.dirname(__file__))
+database_file = os.path.join(basedir, os.environ.get('SQLALCHEMY_DATABASE_URI'))
+database_uri = 'sqlite:///' + database_file
+app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+migrate = Migrate(app, db)
 
 if __name__ == "__main__":
     app.run()
---
+++ server/sqlite.py
@@ -0,0 +1,48 @@
+import sqlite3 
+
+DATABASE = "thewoofster.db"
+
+
+SCHEMA_SQL = """
+    CREATE TABLE IF NOT EXISTS user_type (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        type VARCHAR(20) NOT NULL
+    );
+    
+    CREATE TABLE IF NOT EXISTS contact_info (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        user_type_id INTEGER,
+        fname varchar(10) NOT NULL,
+        lname varchar(10) NOT NULL,
+        primary_phone varchar(10),
+        secondary_phone varchar(10),
+        email varchar(320),
+        street_address varchar(255),
+        city varchar(50),
+        zip varchar(10),
+        FOREIGN KEY (user_type_id) REFERENCES user_type(id) ON DELETE CASCADE
+    );
+    
+    CREATE TABLE IF NOT EXISTS client (
+        id INTEGER PRIMARY KEY AUTOINCREMENT, 
+        contact_info_id INTEGER, 
+        num_pets INTEGER, 
+        notes TEXT, 
+        FOREIGN KEY (contact_info_id) REFERENCES contact_info(id) ON DELETE CASCADE
+    );
+
+"""
+
+
+def init_db():
+    with sqlite3.connect(DATABASE) as conn:
+        conn.executescript(SCHEMA_SQL)  # Run the SQL schema
+        print("Database initialized successfully!")
+
+        # List tables in the database
+        cur = conn.cursor()
+        cur.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cur.fetchall()
+        print("Tables in database:", tables)
+
+init_db()
