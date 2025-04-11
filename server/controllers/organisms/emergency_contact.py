c28eeb29b0858dfaddda7d711e5cc99dcb1d6fe5
lars-codes <lara.palombi@gmail.com>
2025-04-11 14:27:23 -0400

    emergency contact CRUD

--- server/controllers/organisms/emergency_contact.py
+++ server/controllers/organisms/emergency_contact.py
@@ -5,7 +5,6 @@ emergency_contact_bp = Blueprint("emergency_contact", __name__)
 
 @emergency_contact_bp.route('/createEmergencyContact', methods=['POST'])
 def createEmergencyContact():
-    print(request.form)
     relationship = request.form.get('relationship', None)
     fname = request.form.get('fname')
     lname = request.form.get('lname')
@@ -20,16 +19,52 @@ def createEmergencyContact():
     state = request.form.get('state', None)
     zip = request.form.get('zip', None)
     
-    print(user_type)
     try: 
-        res = None 
-        if user_type == 'client':
-            res = EmergencyContact.create_emergency_contact(relationship=relationship, fname=fname, lname=lname, primary_phone=primary_phone, email=email, secondary_phone=secondary_phone, street_address=street_address, city=city, state=state, zip=zip, client_id=id)
-        # elif user_type == 'employee': 
-        #     res = EmergencyContact.create_emergency_contact(relationship=relationship, fname=fname, lname=lname, primary_phone=primary_phone, secondary_phone=secondary_phone, street_address=street_address, city=city, state=state, zip=zip, employee_id=id)
-
+        res = EmergencyContact.create_emergency_contact(user_type=user_type, relationship=relationship, fname=fname, lname=lname, primary_phone=primary_phone, email=email, secondary_phone=secondary_phone, street_address=street_address, city=city, state=state, zip=zip, id=id)
         return res 
 
     except Exception as e:
-        print(f"Unexpected error from /createClient: {e}")
+        print(f"Unexpected error from /createEmergencyContact: {e}")
         return res
+
+@emergency_contact_bp.route('/editEmergencyContact', methods=['PATCH'])
+def editEmergencyContact():
+    # USER TYPE IS REQUIRED AS "client" or "employee"
+    data = {}
+    possible_fields = [
+        'emergency_contact_id', 'user_type', 'relationship', 'fname', 'lname',
+        'primary_phone', 'secondary_phone', 'email', 'street_address', 'city', 'state', 'zip', 'id'
+    ]
+    
+    for field in possible_fields:
+        value = request.form.get(field)
+        if value is not None:
+            data[field] = value
+            
+    if data.get('user_type') == None: 
+        return (
+            jsonify({"success": 0, "error": "Key user_type must be provided (client or employee)"}), 500, 
+        )  
+        
+    try:
+        res = EmergencyContact.edit_emergency_contact(**data)
+
+        return res
+
+    except Exception as e:
+        print(f"Unexpected error from /editEmergencyContact: {e}")
+        return res
+    
+@emergency_contact_bp.route('/deleteEmergencyContact', methods=['DELETE'])
+def deleteEmergencyContact():
+    try: 
+        id = request.form.get('id', 0)
+        user_type = request.form.get('user_type')
+        emergency_contact_id = request.form.get('emergency_contact_id')
+        
+        res = EmergencyContact.delete_emergency_contact(user_type, id, emergency_contact_id)
+        return res 
+    except Exception as e: 
+        print(f"Unexpected error from /deleteEmergencyContact: {e}")
+        return res
+    
--- server/models/organisms/emergency_contact.py
+++ server/models/organisms/emergency_contact.py
@@ -14,37 +14,54 @@ class EmergencyContact(db.Model):
     lname = db.Column(db.String(50), nullable = False)
     relationship = db.Column(db.String(50), nullable=False)
     client_id = db.Column(db.Integer, db.ForeignKey('clients.id'), nullable=True)
-    
+    # employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=True)
+
     contact_info_id = db.Column(db.Integer, db.ForeignKey('contact_info.id'), nullable=False)
 
     contact_info = db.relationship('ContactInfo', lazy='select')
     
     __table_args__ = (
-        db.Index('idx_user', 'client_id'),
+        db.Index('idx_user', 'client_id', 'id'),
     ) 
     
-    def __init__(self, fname, lname, contact_info, user_type, relationship=None, client_id=None):
+    def __init__(self, fname, lname, contact_info, relationship=None, client_id=None, employee_id=None):
         self.fname = fname 
         self.lname = lname 
         self.contact_info = contact_info
-        self.user_type = user_type
         self.client_id = client_id
         self.relationship = relationship
+        # self.employee_id = employee_id
     
 
     @classmethod 
-    def create_emergency_contact(cls, relationship, fname, lname, primary_phone, email=None, secondary_phone=None, street_address=None, city=None, state=None, zip=None, client_id=None):
+    def create_emergency_contact(cls, user_type, relationship, fname, lname, primary_phone, email, secondary_phone, street_address, city, state, zip, id):
         try: 
             contact = ContactInfo(primary_phone=primary_phone, secondary_phone=secondary_phone, email=email, street_address=street_address, city=city, state=state, zip=zip)
             
-            print("CLIENT ID: ", client_id)
-            emergency_contact = cls(
-                fname, 
-                lname, 
-                contact, 
-                relationship, 
-                client_id
-            )
+            if id==0:
+                return jsonify({
+                    "success": 0,
+                    "message": "Client ID or employee ID must be provided user the id key",
+                })
+            
+            
+            emergency_contact = None 
+            if user_type == 'client': 
+                emergency_contact = cls(
+                    fname=fname, 
+                    lname=lname, 
+                    contact_info=contact, 
+                    relationship=relationship, 
+                    client_id=id
+                )
+            elif user_type=="employee":
+                emergency_contact = cls(
+                    fname=fname, 
+                    lname=lname, 
+                    contact_info=contact, 
+                    relationship=relationship, 
+                    employee_id=id
+                )
             
             db.session.add(emergency_contact)
             db.session.commit()
@@ -70,25 +87,88 @@ class EmergencyContact(db.Model):
             )  
         
     @classmethod 
-    def get_emergency_contacts(cls, user_type_id, id): 
-        """
-            If user type id == client: 
-                - search for emergency contact by client id 
-            Else: 
-                - search for emergency contact by employee id 
-        """
-        pass 
+    def edit_emergency_contact(cls, **kwargs):
+        try: 
+            emergency_contact_id = kwargs.get('emergency_contact_id')
+            user_type = kwargs.get('user_type')
+            user_id = kwargs.get('id')
+
+            if user_type == 'client':
+                emergency_contact = cls.query.filter_by(id=emergency_contact_id, client_id=user_id).first()
+            elif user_type == 'employee':
+                emergency_contact = cls.query.filter_by(id=emergency_contact_id, employee_id=user_id).first()
+
+            
+            if not emergency_contact_id:
+                return {'success': False, 'error': 'Emergency contact not found'}
+
+            # Fields directly in EmergencyContact
+            for field in ['relationship', 'fname', 'lname']:
+                if field in kwargs:
+                    setattr(emergency_contact, field, kwargs[field])
+
+            # Related ContactInfo update
+            contact_info = emergency_contact.contact_info
+            if contact_info:
+                for field in ['primary_phone', 'email', 'secondary_phone', 'street_address', 'city', 'state', 'zip']:
+                    if field in kwargs:
+                        setattr(contact_info, field, kwargs[field])
+                        
+            db.session.commit()
+            return jsonify({
+                "success": 1, 
+                "message": "Emergency contact updated succesfully",
+                "emergency_contact_id": emergency_contact_id
+            })
+            
+        except SQLAlchemyError as e: 
+            db.session.rollback()
+            print(f"Database error: {e}")
+            return (
+                jsonify({"success": 0, "error": "Failed to update emergency contact. Database error"}), 500,
+            )    
+    
+        except Exception as e: 
+            db.session.rollback()
+            print(f"Unknown error: {e}")
+            return (
+                jsonify({"success": 0, "error": "Failed to update emergency contact. Unknown error"}), 500, 
+            )  
+        
     
     @classmethod 
-    def update_emergency_contact(cls, relationship, fname, lname, primary_phone=None, primary_email=None, secondary_phone=None, address=None):
-        pass 
+    def delete_emergency_contact(cls, user_type, id, emergency_contact_id):
+        try: 
+            if user_type=='client':
+                emergency_contact = cls.query.filter_by(id=emergency_contact_id, client_id=id).first()
+            elif user_type == 'employee':
+                emergency_contact = cls.query.filter_by(id=emergency_contact_id, employee_id=id).first()
+
+
+            if not emergency_contact:
+                return jsonify({
+                    "success": 0, 
+                    "message": "Emergency contact not found for specified user ID and emergency contact ID",
+                })
+                
+            db.session.delete(emergency_contact)
+            db.session.commit()
+            return jsonify({
+                "success": 1, 
+                "message": "Emergency contact deleted succesfully",
+                "emergency_contact_id": emergency_contact_id
+            })
     
-    @classmethod 
-    def delete_emergency_contact(cls, user_type_id, id, emergency_contact_id):
-        """
-            If user type id == client: 
-                - search for emergency contact by client id 
-            Else: 
-                - search for emergency contact by employee id 
-        """
-        pass 
+        except SQLAlchemyError as e: 
+            db.session.rollback()
+            print(f"Database error: {e}")
+            return (
+                jsonify({"success": 0, "error": "Failed to delete emergency contact. Database error"}), 500,
+            )    
+    
+        except Exception as e: 
+            db.session.rollback()
+            print(f"Unknown error: {e}")
+            return (
+                jsonify({"success": 0, "error": "Failed to delete emergency contact. Unknown error"}), 500, 
+            ) 
