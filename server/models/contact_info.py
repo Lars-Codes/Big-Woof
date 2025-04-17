f21750c2038b858141bf4b625588190f6ace875c
lars-codes <lara.palombi@gmail.com>
2025-04-17 15:16:05 -0400

    phone not required

--- server/models/contact_info.py
+++ server/models/contact_info.py
@@ -4,7 +4,7 @@ class ContactInfo(db.Model):
     
     __tablename__ = 'contact_info'
     id = db.Column(db.Integer, primary_key = True) 
-    primary_phone = db.Column(db.String(10), nullable=False)
+    primary_phone = db.Column(db.String(10))
     secondary_phone = db.Column(db.String(10))
     email = db.Column(db.String(320))
     street_address = db.Column(db.String(255))
