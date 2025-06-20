4500c36dbd28ad2bc55dcf82d4b612674fd3675d
jackgusler <guslerj1@newpaltz.edu>
2025-06-20 11:19:25 -0400

    fix: [BW-134] confirmation alert

--- client/src/components/templates/navigation/Clients/ClientForm/ClientFormHeaderRight.js
+++ client/src/components/templates/navigation/Clients/ClientForm/ClientFormHeaderRight.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import { Text, TouchableOpacity } from 'react-native';
+import { Text, TouchableOpacity, Alert } from 'react-native';
 
 export default function ClientFormHeaderRight({ navigation }) {
+  const handleCancel = () => {
+    Alert.alert('Cancel Form', 'Are you sure you want to close the form?', [
+      {
+        text: 'No',
+        style: 'cancel',
+      },
+      {
+        text: 'Yes',
+        onPress: () => navigation.goBack(),
+      },
+    ]);
+  };
+
   return (
     <TouchableOpacity
-      onPress={() => {
-        navigation.goBack();
-      }}
+      onPress={handleCancel}
       hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }}
     >
       <Text className="text-2xl font-hn-medium text-blue-500">Cancel</Text>
